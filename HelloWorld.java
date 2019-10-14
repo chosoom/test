aa6b4e5f3d4ffb50e04692d678b2127b8ff1a5b3
chosoom <whtnals0619@gmail.com>
2019-10-14 11:09:30 +0900

    Good added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
 	public static void main(Stirng[] args){
 		System.out.println("Hello World");
 		System.out.println("Good Morning");
+		System.out.println("Good");
 	}
 
 }
